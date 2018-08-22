5eed0c89b6d335d421b9d0c9cd18abdfb6de2dc1
pyxisha <xshuang95@gmail.com>
2018-08-22 21:28:55 +0800

    init

---
+++ init.el
@@ -0,0 +1,13 @@
+(setq make-backup-files nil)
+
+(global-set-key (kbd"C-x C-b") 'ibuffer)
+
+(global-linum-mode)
+
+(setq default-tab-width 4)
+
+(setq-default indent-tabs-mode nil)
+
+(setq c-default-style "Linux")
+
+(setq c-basic-offset 4)
